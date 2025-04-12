7d716ec122800c7bfa115a071e2877ab91edf4f1
Edwin Cardenas <ecardenas@lwolf.com>
2025-04-12 13:57:50 -0500

    feat: dynamically set the column padding based on row content

--- Contents.swift
+++ Contents.swift
@@ -11,44 +11,51 @@ protocol TabularDataSource {
 func printTable(_ dataSource: TabularDataSource & CustomStringConvertible) {
     print("Table: \(dataSource)")
     
-    var headerRow = "|"
     var columnWidths = [Int]()
+    var tableContent: [[String]] = []
+    var headerContent = [String]()
     
     for columnIndex in 0..<dataSource.numberOfColumns {
         let label = dataSource.label(forColumn: columnIndex)
-        let columnHeader = " \(label) |"
         
-        headerRow += columnHeader
         columnWidths.append(label.count)
+        headerContent.append(label)
     }
     
-    let sectionSeparator = repeatElement("-", count: headerRow.count).joined()
-    
-    print(sectionSeparator)
-    print(headerRow)
-    print(sectionSeparator)
+    tableContent.append(headerContent)
     
     for rowIndex in 0..<dataSource.numberOfRows {
-        var out = "|"
+        var rowContent = [String]()
         
         for columnIndex in 0..<dataSource.numberOfColumns {
             let item = dataSource.itemFor(row: rowIndex, column: columnIndex)
             var paddingNedded = columnWidths[columnIndex] - item.count
             
             if paddingNedded < 0 {
+                let paddingDifference = abs(paddingNedded)
+                
                 paddingNedded += abs(paddingNedded)
+                columnWidths[columnIndex] += paddingDifference
             }
             
-            let padding = repeatElement(" ", count: paddingNedded)
-                .joined(separator: "")
-            
-            out += " \(item)\(padding) |"
+            rowContent.append(item)
         }
         
-        print(out)
+        tableContent.append(rowContent)
     }
     
-    print(sectionSeparator)
+    let tableContentFormatted = tableContent.map { row in
+        let mainContent = row.enumerated().map {
+            let count = $1.count - columnWidths[$0]
+            let padding = repeatElement(" ", count: abs(count)).joined()
+            
+            return $1 + padding
+        }.joined(separator: " | ")
+        
+        return "| " + mainContent + " |"
+    }
+    
+    print(tableContentFormatted.joined(separator: "\n"))
 }
 
 struct Person {
@@ -100,9 +107,9 @@ struct Department: TabularDataSource, CustomStringConvertible {
 
 var department = Department(name: "Engineering")
 
-department.add(Person(name: "Eva", age: 30, yearsOfExperience: 6))
+department.add(Person(name: "Eva", age: 3000, yearsOfExperience: 6))
 department.add(Person(name: "Saleh", age: 40, yearsOfExperience: 18))
-department.add(Person(name: "Amit", age: 5000, yearsOfExperience: 20))
+department.add(Person(name: "Amit", age: 50000, yearsOfExperience: 20))
 department.add(Person(name: "Edwin", age: 29, yearsOfExperience: 3))
 
 printTable(department)
@@ -169,5 +176,6 @@ var myFavorites = BookCollection(name: "My Favorites")
 
 myFavorites.add(Book(title: "Project Hail Mary", authors: "Andy Weir", averageReview: 5))
 myFavorites.add(Book(title: "So Good They Can't Ignore You", authors: "Cal Newport", averageReview: 5))
+myFavorites.add(Book(title: "Mistborn: The Final Empire", authors: "Brandon Sanderson", averageReview: 5))
 
 printTable(myFavorites)
